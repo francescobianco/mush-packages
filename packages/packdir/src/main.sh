d6431337bff08949455b5eabdba7e44342c13974
francescobianco <info.francescobianco@gmail.com>
2023-09-18 23:51:59 +0200

    First commit

---
+++ packages/packdir/src/main.sh
@@ -0,0 +1,19 @@
+
+main() {
+  local dirpath=$(realpath $1)
+  local dirname=$(dirname $dirpath)
+  local basename=$(basename $dirpath)
+  local packname=$dirname/${basename}_$(date +'%Y%m%d').zip
+  local current_pwd=$PWD
+
+  cd "${dirpath}"
+  echo "Packing ($basename): $dirpath"
+  local first_file=$(find * -type f | head -n 1)
+  #echo "First file: $first_file"
+  zip -or "${packname}" "$first_file"
+  find * -type f | while read file; do
+    zip -uor "${packname}" "${file}"
+  done
+
+  cd "${current_pwd}"
+}
